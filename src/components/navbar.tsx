167e5ddd32b506b1946db4bdb1ee5254fa8631f1
Lelberto <jsurieux@gmail.com>
2020-11-30 15:33:13 +0100

    Remove button (causes errors in console) This button will be readded in the future without error Add margin bottom spacing

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,7 @@ import React from 'react';
  * Navbar component.
  */
 export const Navbar: React.FC = () => (
-    <nav className="bg-secondary-dark">
+    <nav className="mb-3 bg-secondary-dark">
         <div className="px-4">
             <div className="flex items-center justify-between h-16">
                 <div className="flex items-center">
@@ -16,17 +16,6 @@ export const Navbar: React.FC = () => (
                             <NavLink href="#" active>Home</NavLink>
                             <NavLink href="#">Grid</NavLink>
                             <NavLink href="#">Stats</NavLink>
-                        </div>
-                    </div>
-                </div>
-                <div className="hidden md:block">
-                    <div className="ml-4 flex items-center md:ml-6">
-                        <div className="ml-3 relative">
-                            <div>
-                                <button id="user-menu" className="max-w-xs bg-secondary rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-aria-haspopup>
-                                    <img className="w-8 rounded-full" src="https://icons-for-free.com/iconfiles/png/512/fireworks+new+year+party+icon-1320185813780531454.png" />
-                                </button>
-                            </div>
                         </div>
                     </div>
                 </div>
