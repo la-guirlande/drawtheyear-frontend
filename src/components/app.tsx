0caf4c8b1c9d64a273f0a0fa4511acb602787312
Lelberto <jsurieux@gmail.com>
2020-11-30 15:29:45 +0100

    Add home page component and implementation

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { HomePage } from './homepage';
+import { Navbar } from './navbar';
 
 const App: React.FC = () => (
-    <></>
+    <>
+        <Navbar />
+        <HomePage />
+    </>
 );
 
 export default App;
