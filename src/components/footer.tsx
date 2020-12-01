2da2d17ef5de68145e02a5d5cf218107ceb9a191
Lelberto <jsurieux@gmail.com>
2020-12-01 13:02:18 +0100

    Update footer Add Faichyer in the copyright line Add colors on names

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,6 +7,6 @@ import React from 'react';
 export const Footer: React.FC = () => (
     <footer className="text-sm text-center">
         <p>Copyright © DTY {moment(new Date()).format('YYYY')}</p>
-        <p>Made with <span className="text-red-500">♥</span> by Lelberto</p>
+        <p>Made with <span className="text-red-500">♥</span> by <span className="text-primary-light">Lelberto</span> and <span className="text-primary-light">Faichyer</span></p>
     </footer>
 )
