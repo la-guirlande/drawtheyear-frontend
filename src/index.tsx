3d27448b2e134143a6fc0b046592b5ef2495a1f8
Lelberto <jsurieux@gmail.com>
2020-12-20 22:00:50 +0100

    Change tailwind build configuration Change CSS target Change `<link />` to `import`

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import reportWebVitals from './reportWebVitals';
+import './assets/css/index.css';
 
 ReactDOM.render(
     <React.StrictMode>
