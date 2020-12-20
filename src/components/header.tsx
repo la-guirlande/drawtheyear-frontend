45b0d51890a3215b0147f27132e7cedbef22b963
Lelberto <jsurieux@gmail.com>
2020-12-20 22:01:35 +0100

    Add font primary on header title

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import React from 'react';
  */
 export const Header: React.FC = () => (
     <header className="text-center">
-        <h1 className="text-5xl">DrawTheYear</h1>
+        <h1 className="text-5xl font-primary">DrawTheYear</h1>
         <h2 className="text-2xl">Online <span className="text-primary">Year in pixels</span></h2>
     </header>
 )
