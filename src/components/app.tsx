d2ef62dfd11d796d01b7d6453af54c3b76b50858
Lelberto <jsurieux@gmail.com>
2020-12-21 15:47:36 +0100

    Add navbar and nav components Move navbar components to their folder

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HomePage } from './homepage';
-import { Navbar } from './navbar';
+import { Navbar } from './navbar/navbar';
 
 const App: React.FC = () => (
     <>
---
+++ src/components/navbar/nav-link.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+/**
+ * Navbar link props.
+ */
+interface NavLinkProps {
+    href: string;
+    active?: boolean;
+}
+
+/**
+ * Navbar link component.
+ * 
+ * @param href Href
+ * @param active True to render link as active
+ */
+export const NavLink: React.FC<NavLinkProps> = (props) => {
+    return props.active ? (
+        <a href={props.href} className="px-3 py-2 rounded-md font-medium bg-primary-dark hover:bg-primary waves-effect">{props.children}</a>
+    ) : (
+        <a href={props.href} className="px-3 py-2 rounded-md font-medium hover:bg-secondary waves-effect">{props.children}</a>
+    )
+}
