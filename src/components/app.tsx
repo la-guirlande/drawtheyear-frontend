74c3af3b320fe93976759b0b7329defbe9097d0a
Lelberto <jsurieux@gmail.com>
2021-01-29 19:59:05 +0100

    Add router

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { HomePage } from './homepage';
 import { Navbar } from './navbar/navbar';
 
 const App: React.FC = () => (
-    <>
-        <Navbar />
-        <HomePage />
-    </>
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/">
+                <Navbar />
+                <HomePage />
+            </Route>
+            <Route exact path="/grid">
+                <Navbar />
+            </Route>
+            <Route exact path="/stats">
+                <Navbar />
+            </Route>
+            <Route exact path="/signin">
+                <Navbar />
+            </Route>
+        </Switch>
+    </BrowserRouter>
 );
 
 export default App;
