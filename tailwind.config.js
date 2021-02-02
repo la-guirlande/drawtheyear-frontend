04fab650eb057f22faf6db495c3300ceb88ec2ac
Lelberto <jsurieux@gmail.com>
2021-02-02 18:23:34 +0100

    Add big box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,9 @@ module.exports = {
                 light: '#fff4e4',
                 dark: '#3d0e1e'
             },
+            boxShadow: {
+                big: '25px 0 50px -12px rgba(0, 0, 0, 0.75);'
+            },
             textShadow: {
                 cell: '-1px 0 1px rgb(50, 50, 50), 0 1px 1px rgb(50, 50, 50), 1px 0 1px rgb(50, 50, 50), 0 -1px 1px rgb(50, 50, 50)'
             },
