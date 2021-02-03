0ba4b25fdfe68a121ab044fb1d5e35adadf31ae1
Lelberto <jsurieux@gmail.com>
2021-02-03 20:02:26 +0100

    Refactoring cells

--- src/components/grid/day-cell.tsx
+++ src/components/grid/day-cell.tsx
@@ -28,9 +28,7 @@ export const DayCell: React.FC<DayCellProps> = ({ dayofmonth, hoverText, day, ch
   return (
     <div className="z-0 relative transition duration-500 ease-in-out transform hover:z-10 hover:scale-125" onMouseEnter={toggleDetail} onMouseLeave={toggleDetail}>
       <a className="w-auto flex items-center justify-center h-8 border border-solid border-black rounded text-center cursor-pointer" style={{ backgroundColor: day.emotions[0].color }} {...rest}>
-        <span className="text-shadow-cell">
-          {showHoverText ? hoverText : dayofmonth}
-        </span>
+        <span className="text-shadow-cell">{showHoverText ? hoverText : dayofmonth}</span>
       </a>
     </div>
   );
--- src/components/grid/no-day-cell.tsx
+++ src/components/grid/no-day-cell.tsx
@@ -23,11 +23,9 @@ export const NoDayCell: React.FC<NoDayCellProps> = ({ dayofmonth, hoverText, chi
   const toggleCreate = () => setShowHoverText(!showHoverText);
 
   return (
-    <div className="z-0 relative transition duration-500 ease-in-out transform hover:z-10 hover:scale-125"  onMouseEnter={toggleCreate} onMouseLeave={toggleCreate}>
+    <div className="z-0 relative transition duration-500 ease-in-out transform hover:z-10 hover:scale-125" onMouseEnter={toggleCreate} onMouseLeave={toggleCreate}>
       <a className="flex items-center justify-center h-8 border border-solid border-black rounded text-center cell-empty cursor-pointer" {...rest}>
-        <span>
-          {showHoverText ? hoverText : dayofmonth}
-        </span>
+        <span>{showHoverText ? hoverText : dayofmonth}</span>
       </a>
     </div>
   );
