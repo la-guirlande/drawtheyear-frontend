bebd2a4d5129f9411241812861991416cbff0305
Lelberto <jsurieux@gmail.com>
2021-02-03 14:12:46 +0100

    Add dynamic hover text on cells

--- src/components/grid/day-cell.tsx
+++ src/components/grid/day-cell.tsx
@@ -3,19 +3,20 @@ import { DayData } from '../../util/types/data-types';
 
 export interface DayCellProps extends React.HTMLProps<HTMLAnchorElement> {
   dayofmonth: number;
+  hoverText: string;
   day: DayData;
 }
 
-export const DayCell: React.FC<DayCellProps> = ({ dayofmonth, day, children, ...rest }) => {
-  const [showDetailText, setShowDetailText] = useState(false);
+export const DayCell: React.FC<DayCellProps> = ({ dayofmonth, hoverText, day, children, ...rest }) => {
+  const [showHoverText, setShowHoverText] = useState(false);
 
-  const toggleDetail = () => setShowDetailText(!showDetailText);
+  const toggleDetail = () => setShowHoverText(!showHoverText);
 
   return (
     <div className="z-0 relative transition duration-500 ease-in-out transform hover:z-10 hover:scale-125" onMouseEnter={toggleDetail} onMouseLeave={toggleDetail}>
       <a className="w-auto flex items-center justify-center h-8 border border-solid border-black rounded text-center cursor-pointer" style={{ backgroundColor: day.emotions[0].color }} {...rest}>
         <span className="text-shadow-cell">
-          {showDetailText ? 'Détails' : dayofmonth}
+          {showHoverText ? hoverText : dayofmonth}
         </span>
       </a>
     </div>
--- src/components/grid/empty-cell.tsx
+++ src/components/grid/empty-cell.tsx
@@ -2,18 +2,19 @@ import { useState } from 'react';
 
 export interface EmptyCellProps extends React.HTMLProps<HTMLAnchorElement> {
   dayofmonth: number;
+  hoverText: string;
 }
 
-export const EmptyCell: React.FC<EmptyCellProps> = ({ dayofmonth, children, ...rest }) => {
-  const [showCreateText, setShowCreateText] = useState(false);
+export const EmptyCell: React.FC<EmptyCellProps> = ({ dayofmonth, hoverText, children, ...rest }) => {
+  const [showHoverText, setShowHoverText] = useState(false);
 
-  const toggleCreate = () => setShowCreateText(!showCreateText);
+  const toggleCreate = () => setShowHoverText(!showHoverText);
 
   return (
     <div className="z-0 relative transition duration-500 ease-in-out transform hover:z-10 hover:scale-125"  onMouseEnter={toggleCreate} onMouseLeave={toggleCreate}>
       <a className="flex items-center justify-center h-8 border border-solid border-black rounded text-center cell-empty cursor-pointer" {...rest}>
         <span>
-          {showCreateText ? 'Créer' : dayofmonth}
+          {showHoverText ? hoverText : dayofmonth}
         </span>
       </a>
     </div>
