17fd2eb27c3a01b6ea60c811a76ed6719be4d5d1
Lelberto <jsurieux@gmail.com>
2021-02-08 14:27:35 +0100

    Update stats Add directory per stat type Add general stats with a global and a monthly emotion distribution graphs

--- src/components/stats/stats-container.tsx
+++ src/components/stats/stats-container.tsx
@@ -1,53 +1,36 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { AuthenticationContext } from '../contexts/authentication-context'
-import { ChartData, EmotionsDistribution } from './emotions-distribution';
+import { GeneralContainer } from './general/general-container';
+import { StatsSidebar, StatType } from './stats-sidebar';
 
 export const StatsContainer: React.FC = () => {
   const { authUser } = useContext(AuthenticationContext);
-  const [globalData, setGlobalData] = useState<ChartData[]>([]);
-  const [monthData, setMonthData] = useState<ChartData[]>([]);
-  const [month, setMonth] = useState(0);
+  const [statType, setStatType] = useState<StatType>('general');
 
-  useEffect(() => {
-    if (authUser) {
-      setGlobalData(authUser.emotions.map(emotion => ({
-        id: emotion.name,
-        label: emotion.name,
-        color: emotion.color,
-        value: authUser.days.filter(day => day.emotions.map(emotion => emotion.id).includes(emotion.id)).length
-      })));
-    }
-  }, [authUser]);
+  const handleChangeStatType = (type: StatType) => {
+    setStatType(type);
+  }
 
-  useEffect(() => {
-    if (authUser) {
-      setMonthData(authUser.emotions.map(emotion => ({
-        id: emotion.name,
-        label: emotion.name,
-        color: emotion.color,
-        value: authUser.days.filter(day => new Date(day.date).getMonth() === month && day.emotions.map(emotion => emotion.id).includes(emotion.id)).length
-      })));
+  const generateView = () => {
+    switch (statType) {
+      default:
+      case 'general':
+        return <GeneralContainer />
+      case 'other':
+        return 'Another stat';
     }
-  }, [authUser, month]);
+  }
 
   return authUser && (
-    <div className="container h-screen">
-      <EmotionsDistribution data={globalData} />
-      <select onChange={e => setMonth(parseInt(e.target.value))}>
-        <option value={0}>Janvier</option>
-        <option value={1}>Février</option>
-        <option value={2}>Mars</option>
-        <option value={3}>Avril</option>
-        <option value={4}>Mai</option>
-        <option value={5}>Juin</option>
-        <option value={6}>Juillet</option>
-        <option value={7}>Août</option>
-        <option value={8}>Septembre</option>
-        <option value={9}>Octobre</option>
-        <option value={10}>Novembre</option>
-        <option value={11}>Décembre</option>
-      </select>
-      <EmotionsDistribution data={monthData} />
+    <div className="h-screen flex flex-wrap">
+      <div className="w-1/5">
+        <StatsSidebar onSelect={handleChangeStatType} />
+      </div>
+      <div className="w-4/5 px-2">
+        <div className="mx-2">
+          {generateView()}
+        </div>
+      </div>
     </div>
   );
 }
