6c0d3caeab83acd351face78a49f82c6da819577
Rocknologie <rocknologie@hotmail.fr>
2021-02-13 15:02:38 +0100

    Appel des cellules & update grid

--- src/components/views/monthly/monthly.tsx
+++ src/components/views/monthly/monthly.tsx
@@ -1,11 +1,10 @@
 import moment from 'moment';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { DayData, UserData } from '../../../util/types/data-types';
-import { EmptyCell } from '../../grid/empty-cell';
-import { NoDayCell } from '../../grid/no-day-cell';
-import { MonthlyCalendar } from './monthly-calendar';
 import { MonthlyDay } from './monthly-day';
+import { MonthlyEmptyCell } from './monthly-empty-cell';
 import { MonthlyMonth } from './monthly-month';
+import { MonthlyNoDayCell } from './monthly-no-day-cell';
 
 /**
  * Grid props.
@@ -42,16 +41,16 @@ export const Monthly: React.FC<MonthlyProps> = ({ user, year, editable, onDaySel
         }
         return colDays.map(({ row, day }, i) => {
             if (cellNotDay(row, month)) {
-                return <EmptyCell key={`${month}-${i}`} />
+                return <MonthlyEmptyCell key={`${month}-${i}`} />
             }
             if (day) {
                 return <MonthlyDay key={`${month}-${i}`} dayofmonth={row} day={day} hoverText="Détails" onClick={() => onDaySelect(day)} />
             }
             const dateStr = moment(`${year}-${month}-${row}`).format('YYYY-MM-DD');
             if (editable) {
-                return <NoDayCell key={`${month}-${i}`} dayofmonth={row} hoverText="Créer" href={`/grid/${user.name}/day/add?date=${dateStr}`} />
+                return <MonthlyNoDayCell key={`${month}-${i}`} dayofmonth={row} hoverText="Créer" href={`/grid/${user.name}/day/add?date=${dateStr}`} />
             }
-            return <NoDayCell key={`${month}-${i}`} dayofmonth={row} hoverText="-----" />
+            return <MonthlyNoDayCell key={`${month}-${i}`} dayofmonth={row} hoverText="-----" />
         });
     }
 
@@ -60,12 +59,14 @@ export const Monthly: React.FC<MonthlyProps> = ({ user, year, editable, onDaySel
         return (column === 2 && row > (isLeap ? 29 : 28)) || ((column === 4 || column === 6 || column === 9 || column === 11) && row > 30) || (row > 31) || (column > 12);
     }
 
+    // TODO Faire passer le mois pour l'afficher.
     const generateToutCours = (countMonth: number) => {
         const element: JSX.Element[] = [];
         for (let i = 1; i <= countMonth; i++) {
-            element.push(<div className="w-full inline-block p-20">
-                <div className="grid grid-flow-row auto-rows-max">
-                    <h1 className="text-secondary">Mois {i}</h1>
+            element.push(<div className="w-full inline-block p-10">
+                <h1 className="text-secondary">Mois {i}</h1>
+                <div className="grid grid-cols-5">
+                    {/* TODO Ajouter le margin left dans la génération du calendar toutes les 2 lignes. */}
                     {generateCalendar(i)}
                 </div>
             </div>)
@@ -74,14 +75,16 @@ export const Monthly: React.FC<MonthlyProps> = ({ user, year, editable, onDaySel
     }
 
     return (
-        <div className="grid grid-cols-3 gap-4">
-            {/* CHAQUE MOIS POSSEDE TOUTE LA SECTION SUIVANTE */}
-            <div className="bg-black flex justify-center items-center">
-                <MonthlyMonth />
-            </div>
-            {/* Contenue de la section droite de la page (contenu du calendrier) */}
-            <div className="col-span-2 bg-white">
-                {generateToutCours(12)}
+        <div>
+            <div className="grid grid-cols-4 gap-2" style={{ backgroundColor: "red"}}>
+                {/* TODO CHAQUE MOIS POSSEDE TOUTE LA SECTION SUIVANTE */}
+                <div className="flex col-span-1 justify-center items-center bg-black">
+                    <MonthlyMonth />
+                </div>
+                {/* Contenue de la section droite de la page (contenu du calendrier) */}
+                <div className="col-span-3 bg-white">
+                    {generateToutCours(12)}
+                </div>
             </div>
         </div>
     )
