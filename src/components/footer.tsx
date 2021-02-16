cc0c678f270dba046f9f800054319c8946ba98e5
Miki Namsrai <miki.namsrai@gmail.com>
2021-02-16 09:37:28 +0100

    correct footer bug

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,8 +5,8 @@ import React from 'react';
  * Footer component.
  */
 export const Footer: React.FC = () => (
-  <div className="container mx-auto bottom-0 absolute">
-    <hr className="my-3 bg-light ml-16"></hr>
+  <div className="w-full mx-auto bottom-0 absolute">
+    <hr className="my-3 bg-light"></hr>
     <footer className="text-sm text-center">
       <p>Copyright © DTY {moment(new Date()).format('YYYY')}</p>
       <p>Made with <span className="text-red-500">♥</span> by the <span className="text-primary-light">CobaTeam</span></p>
