0742a4adb5961372551a4249942d8bd8f62d3f31
Rocknologie <rocknologie@hotmail.fr>
2021-02-23 15:50:46 +0100

    Change for PR

--- src/components/views/monthly/month-cells/month-empty-cell.tsx
+++ src/components/views/monthly/month-cells/month-empty-cell.tsx
@@ -5,8 +5,7 @@
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const MonthEmptyCell: React.FC = ({ children, ...rest }) => (
-    <div className="m-2">
-      <a className="flex items-center justify-center h-8 border border-solid border-black rounded-tl-lg rounded-br-lg text-center cell-no-day" {...rest}></a>
-    </div>
-  ); 
-  
+  <div className="m-2">
+    <a className="flex items-center justify-center h-8 border border-solid border-black rounded-tl-lg rounded-br-lg text-center cell-no-day" {...rest}></a>
+  </div>
+);
