8ebdbf364204b505a8e9f48cf80efedca6977ad9
Lelberto <jsurieux@gmail.com>
2021-02-24 12:37:27 +0100

    Move routing code to the router component

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { AuthenticationContextProvider } from './contexts/authentication-context';
-import { Footer } from './footer';
-import { GridPage } from './grid/grid-page';
-import { HomePage } from './home/home-page';
-import { Navbar } from './navbar/navbar';
-import { SigninPage } from './signin/signin-page';
-import { SignupPage } from './signup/signup-page';
-import { StatsPage } from './stats/stats-page';
+import { Router } from './router';
 
 /**
  * App component.
@@ -17,25 +11,7 @@ import { StatsPage } from './stats/stats-page';
 const App: React.FC = () => (
   <AuthenticationContextProvider>
     <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route exact path="/grid/:username">
-          <GridPage />
-        </Route>
-        <Route exact path="/stats">
-          <StatsPage />
-        </Route>
-        <Route exact path="/signin">
-          <SigninPage />
-        </Route>
-        <Route exact path="/signup">
-          <SignupPage />
-        </Route>
-      </Switch>
-      <Footer />
+      <Router />
     </BrowserRouter>
   </AuthenticationContextProvider>
 );
