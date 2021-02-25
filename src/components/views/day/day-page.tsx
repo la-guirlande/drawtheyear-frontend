c9347c852f399c83ce97b7c9293265b7555c9d6f
rocknologie <rocknologie@hotmail.fr>
2021-02-25 18:06:38 +0100

    Day page

---
+++ src/components/views/day/day-page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { DayContainer } from './day-container';
+
+/**
+ * Day page component.
+ */
+export const DayPage: React.FC = () => {
+  return (
+    <div className="mt-3">
+      <DayContainer />
+    </div>
+  );
+}
