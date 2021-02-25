1ed822f6e95be6c31a6ca8c8c1f139b1ff68ce7c
Miki Namsrai <miki.namsrai@gmail.com>
2021-02-25 21:14:34 +0100

    left side done

--- src/components/signin/signin-container.tsx
+++ src/components/signin/signin-container.tsx
@@ -7,6 +7,7 @@ import { GetUserResponse, RefreshTokenResponse } from '../../util/types/response
 import { Button } from '../button';
 import { AuthenticationContext } from '../contexts/authentication-context';
 import { SigninForm, SigninFormValues } from './signin-form';
+import Daydotted from '../../assets/img/daydotted.png'
 
 /**
  * Signin container.
@@ -52,9 +53,32 @@ export const SigninContainer: React.FC = () => {
   }
 
   return (
-    <>
-      <SigninForm onSubmit={handleSubmitSigninForm} />
-      <Button href="/signup">Inscription</Button>
-    </>
+    <div className="grid grid-cols-5 gap-4 w-full">
+      <div className="col-span-3">
+        <div className="flex flex-col justify-center ">
+          <div className="flex flex-col mx-auto my-0">
+            <div className="text-5xl pt-36">
+              Sign In to
+            </div>
+            <div className="text-5xl ">
+              Start writing
+            </div>
+            <div className="text-xl mt-8 flex flex-col text-gray-400">
+              If you don't have an account
+            </div>
+            <div className="text-gray-400 mx-auto">
+              You can <span className="text-yellow-600 hover:border-b-2 border-solid hover:border-yellow-500 hover:text-yellow-500 cursor-pointer">Register here</span>
+            </div>
+            <div className="w-80">
+              <img src={Daydotted} alt="Day dotted"/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col-span-2">
+        <SigninForm onSubmit={handleSubmitSigninForm} />
+        <Button href="/signup">Inscription</Button>
+      </div>
+    </div>
   );
 }
