8ec5bf04904fc49e75637f5a7bb681e283c90153
Miki Namsrai <miki.namsrai@gmail.com>
2021-09-11 18:02:01 +0200

    erase old dty

--- src/components/badge.tsx
+++
@@ -1,8 +0,0 @@
-import { FC, HTMLProps } from 'react';
-
-/**
- * Badge component. 
- */
-export const Badge: FC<HTMLProps<HTMLDivElement>> = ({ children, ...rest }) => (
-  <div className="inline-block px-2 py-1 rounded-2xl text-sm border-2 border-opacity-30 border-light cursor-default text-shadow-cell transition hover:border-opacity-70" {...rest}>{children}</div>
-);
