7277de2a1d1dd8ad92d28e45e114d363c9b6da12
Lelberto <jsurieux@gmail.com>
2021-11-03 17:48:45 +0100

    Add fonts and theme base configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class', // false or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'quicksand-book': ['Quicksand-Book'],
+        'extreme': ['Extreme']
+      },
+      colors: {
+        primary: {
+          light: '#D5f7e6',
+          DEFAULT: '#B5FFD9',
+          dark: '#40ad77'
+        },
+        secondary: {
+          light: '#D272fd',
+          DEFAULT: '#AE00FB',
+          dark: '#8004b7'
+        }
+      }
+    }
   },
   variants: {
     extend: {},
