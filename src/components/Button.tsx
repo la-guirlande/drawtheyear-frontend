1447d62a750cb9fa1b3da5afdcd18c84c7938fb4
Lelberto <jsurieux@gmail.com>
2021-11-03 20:02:22 +0100

    Update button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -19,8 +19,8 @@ export const Button: FC<ButtonProps> = ({ className, variant, outline, size, chi
     'px-4', { 'py-1': size === 'sm' }, { 'py-2': size === 'md' }, { 'py-3': size === 'lg' },
     'font-quicksand-book',
     'rounded-sm',
-    `ring-4 ring-offset-1 ring-${variant}-dark ring-opacity-0 focus:ring-opacity-100`,
-    { [`ring-offset-${variant}-dark dark:ring-offset-${variant}-light focus:ring-offset-white`]: outline },
+    `ring-4 ring-${variant}-dark ring-opacity-0 focus:ring-opacity-100`,
+    { [`ring-offset-1 ring-offset-${variant}-dark dark:ring-offset-${variant}-light focus:ring-offset-white`]: outline },
     { [`bg-${variant}-dark`]: !outline }, { [`bg-${variant}-dark bg-opacity-0`]: outline }, 'hover:bg-opacity-75 focus:bg-opacity-75',
     { 'text-white': !outline }, { [`text-${variant}-dark dark:text-${variant}-light hover:text-white focus:text-white`]: outline },
     'transition-all duration-200'
